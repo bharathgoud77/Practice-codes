f23f3a35b591a75006f3a38d5830b156b99a310b
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-02 04:57:40 -0700

    hormonic number

---
+++ HarmonicNum/HarmonicNum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonicNum
+{
+    using System;
+
+    class Number
+    {
+
+        
+        static double HarmonicNum(int N)
+        {
+            
+            float harmonic = 1;
+
+            for (int i = 2; i <= N; i++)
+            {
+                harmonic += (float)1 / i;
+            }
+
+            return harmonic;
+        }
+
+        static public void Main()
+        {
+            int N = 4;
+
+            Console.Write(HarmonicNum(N));
+            Console.ReadKey();
+        }
+    }
+
+}
