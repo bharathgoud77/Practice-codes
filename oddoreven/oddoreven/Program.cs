68edf1b1174ee727c1ed423490a1f4b945a4970e
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-02 05:37:48 -0700

    it checks given number is odd or even

---
+++ oddoreven/oddoreven/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oddoreven
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n;
+            Console.WriteLine("Enter a number to check : ");
+            n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+               
+                Console.WriteLine(n + " is an even number");
+            }
+            else
+            {
+                
+                Console.WriteLine(n + " is an odd number");
+                Console.ReadKey();
+            }
+        }
+    }
+}
